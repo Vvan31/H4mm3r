edd90f7b6a535a8e3d8534b8dcea864fb945651c
VivianOsorio <v31.osorio@gmail.com>
2023-02-24 23:30:35 -0800

    fixed async problem loading models

--- vite-project/main.js
+++ vite-project/main.js
@@ -2,20 +2,21 @@ import * as THREE from 'three';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
 
 let scene, camera, renderer; 
+scene = new THREE.Scene();
+const loader = new GLTFLoader();
+let shark_mixer = await loadModel(loader, "media/hammerhead.glb", 3.8, 0, 0, 0, 0, -65,-10,-10,-10, 'Action', false);
+let ray_mixer = await loadModel(loader, "media/manta.glb", 3.8, 0, 0, 30, 0, -65,-1,-1,-1, 'swim', false);
+let water_mixer = await loadModel(loader, "media/water_animation.glb", 0.8, 0, 0, 0, 0, -63,4,8,5 , 'Take 001', true);
 
-async function main(){
+function main(){
   createScene();
   addLights();
-    let shark_mixer, ray_mixer, water_mixer = await load3DObjects();
-    console.log("mixer::::");
-    console.log(shark_mixer);
-    console.log("::::mixer");
-
-    animate(shark_mixer, ray_mixer, water_mixer);
+/*   changeWaterMesh(); */
+  animate();
 }
 function createScene(){
   // Scene: The scene is where all the 3D objects will be displayed.
-  scene = new THREE.Scene();
+  /* scene = new THREE.Scene(); */
 
   // Camera:  The camera determines what part of the scene is visible.
   camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
@@ -38,28 +39,8 @@ function addLights(){
   scene.add( dirLight );
 }
 
-async function load3DObjects(){
-  const loader = new GLTFLoader();
-  // loader --- Path ---- rotation: x, y, z --- position: x, y, z ---- scale. 
-  let shark = await loadModel(loader, "media/hammerhead.glb", 3.8, 0, 0, 0, 0, -65,-10,-10,-10);
-  let ray = await loadModel(loader, "media/manta.glb", 3.8, 0, 0, 30, 0, -65,-10,-10,-10);
-  let water = await loadModel(loader, "media/water_animation.glb", 0.8, 0, 0, 0, 0, -63,4,8,5);
-  setTimeout(20000)
-  const shark_mixer = setTimeout(animateModel(shark, 'Action'), 900000);
-  const ray_mixer = setTimeout(animateModel(ray, 'swim'), 900000);
-  const water_mixer = setTimeout(animateModel(water, 'Take 001'), 90000);
-
-  changeWaterMesh(water);
-
-  scene.add(shark.scene);
-  scene.add(water.scene);
-  scene.add(ray.scene);
-
-  return shark_mixer, ray_mixer, water_mixer;
- }
-
- async function loadModel(loader, path, rot_x, rot_y, rot_z, pos_x, pos_y, pos_z, sc1,sc2, sc3){
-  const model = await loader.loadAsync(path);
+ async function loadModel(loader, path, rot_x, rot_y, rot_z, pos_x, pos_y, pos_z, sc1,sc2, sc3, name, change){
+  let model = await loader.loadAsync(path);
   model.scene.rotation.x = rot_x;
   model.scene.rotation.y = rot_y;
   model.scene.rotation.z = rot_z;
@@ -67,7 +48,14 @@ async function load3DObjects(){
   model.scene.position.y = pos_y;
   model.scene.position.z = pos_z;
   model.scene.scale.set(sc1, sc2, sc3); 
-  return model; 
+  console.log(model);
+  // animation mixers
+  const mixer = animateModel(model, name);
+  scene.add(model.scene);
+  if (change) {
+    changeWaterMesh(model);
+  }
+  return model, mixer; 
 }
 
 function animateModel(model, actionName){
@@ -79,6 +67,7 @@ function animateModel(model, actionName){
   return mixer;
 }
 function changeWaterMesh(water){
+  console.log(water);
   let meshes = water.parser.associations.keys();
   for (let i = 0; i <= 15; i++) {
     const material = meshes.next().value;
@@ -92,7 +81,7 @@ function changeWaterMesh(water){
 }
 
 //Render the scene: This will display the 3D object on the screen.
-function animate(shark_mixer, ray_mixer, water_mixer) {
+function animate() {
    // animate model
    shark_mixer.update( 0.015 );
    ray_mixer.update( 0.015 );
